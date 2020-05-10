3f0f714eda18ad5944655f6c14172060515e8b5c
dcribb19 <dcribb19@users.noreply.github.com>
2020-05-10 11:16:07 -0400

    submission Bite 21 @codechalleng.es

---
+++ 21/cars.py
@@ -0,0 +1,46 @@
+import re
+
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+
+
+def get_all_jeeps(cars=cars):
+    """return a comma + space (', ') separated string of jeep models
+       (original order)"""
+    jeeps = ''
+    
+    for make in cars.keys():
+        if make == 'Jeep':
+            for model in cars[make]:
+                if model == cars[make][-1]:
+                    jeeps += model
+                else:
+                    jeeps += model + ", "
+    return jeeps
+
+
+def get_first_model_each_manufacturer(cars=cars):
+    """return a list of matching models (original ordering)"""
+    first_models = [cars[make][0] for make in cars.keys()]
+    return first_models
+
+
+def get_all_matching_models(cars=cars, grep='trail'):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    grep_finder = re.compile(grep, re.IGNORECASE)
+    matching_models = sorted([model for models in cars.values() for model in models if grep_finder.findall(model)])
+    return matching_models
+
+
+def sort_car_models(cars=cars):
+    """return a copy of the cars dict with the car models (values)
+       sorted alphabetically"""
+    cars = {make: sorted(models) for make, models in cars.items()}
+    return cars
