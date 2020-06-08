5930e2b35f73c65f300760178a68be272e49bbe9
dcribb19 <daniel.cribb.10@gmail.com>
2020-06-07 23:04:52 -0400

    submission Bite 74 @codechalleng.es

--- 74/bday.py
+++ 74/bday.py
@@ -4,18 +4,5 @@ from datetime import date
 
 def weekday_of_birth_date(date):
     """Takes a date object and returns the corresponding weekday string"""
-    d = date.weekday()
-    if d == 0:
-        return 'Monday'
-    elif d == 1:
-        return 'Tuesday'
-    elif d == 2:
-        return 'Wednesday'
-    elif d == 3:
-        return 'Thursday'
-    elif d == 4:
-        return 'Friday'
-    elif d == 5:
-        return 'Saturday'
-    else:
-        return 'Sunday'
+    day = date.weekday()
+    return calendar.day_name[day]
