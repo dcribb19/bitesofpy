e867a1d84a724378db21779dc116f841b63f0939
dcribb19 <daniel.cribb.10@gmail.com>
2020-06-21 20:50:53 -0400

    submission Bite 257 @codechalleng.es

---
+++ 257/users.py
@@ -0,0 +1,37 @@
+import re
+
+pw4 = """
+mysql:x:106:107:MySQL Server,,,:/var/lib/mysql:/bin/false
+avar:x:1000:1000::/home/avar:/bin/bash
+chad:x:1001:1001::/home/chad:/bin/bash
+git-svn-mirror:x:1002:1002:Git mirror,,,:/home/git-svn-mirror:/bin/bash
+gerrit2:x:1003:1003:Gerrit User,,,:/home/gerrit2:/bin/bash
+avahi:x:107:108:Avahi mDNS daemon,,,:/var/run/avahi-daemon:/bin/false
+postfix:x:108:112::/var/spool/postfix:/bin/false
+ssh-rsa:x:1004:1004::/home/ssh-rsa:/bin/bash
+artagnon:x:1005:1005:Ramkumar R,,,,Git GSOC:/home/artagnon:/bin/bash
+"""
+
+def get_users(passwd: str) -> dict:
+    """Split password output by newline,
+      extract user and name (1st and 5th columns),
+      strip trailing commas from name,
+      replace multiple commas in name with a single space
+      return dict of keys = user, values = name.
+    """
+    users = {}
+    passwd = passwd.splitlines()
+    for line in passwd:
+        if line == '':
+            continue
+        line = line.split(':')
+        user = line[0]
+        name = line[4]
+        if name == '':
+          name = 'unknown'
+        if name.endswith(','):
+            name = name.rstrip(',')
+        name = re.sub(r'[,]+', ' ', name)
+        users[user] = name
+
+    return users
