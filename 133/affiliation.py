b499272dfec916469d7c12d86708b8866a91f04c
dcribb19 <daniel.cribb.10@gmail.com>
2020-06-22 21:15:04 -0400

    submission Bite 133 @codechalleng.es

--- 133/affiliation.py
+++ 133/affiliation.py
@@ -1,10 +1,5 @@
 import re
 
-url = 'https://www.amazon.com/War-Art-Through-Creative-Battles/dp/1936891026/?keywords=war+of+art'
-url4 = 'https://www.amazon.co.uk/Pragmatic-Programmer-Andrew-Hunt/dp/020161622X'
-url5 = 'https://www.amazon.com.au/Python-Cookbook-3e-David-Beazley/dp/1449340377/'
-url6 = 'https://www.amazon.com/fake-book-with-longer-asin/dp/1449340377000/'
-
 def generate_affiliation_link(url):
     url = re.sub(r'\S+/dp/', 'http://www.amazon.com/dp/', url)
     end = re.split(r'\S+/dp/\d+\w?', url)
