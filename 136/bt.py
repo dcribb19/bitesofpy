29e8d304625d09207a92913fc313333a53e07707
dcribb19 <daniel.cribb.10@gmail.com>
2020-06-26 16:58:19 -0400

    submission Bite 136 @codechalleng.es

---
+++ 136/bt.py
@@ -0,0 +1,109 @@
+"""
+Write a function which checks the red blood cell compatibility between donor and recipient.
+https://en.wikipedia.org/wiki/Blood_type#Red_blood_cell_compatibility
+For simplicity, the appearance of 8 basic types of blood is considered.
+The input of blood type can be in the form of:
+    pre defined Bloodtype enum e.g.: Bloodtype.ZERO_NEG
+    value of the pre-defined Bloodtype 0..7
+    pre defined text  e.g. "0-", "B+", "AB+", ...
+    If input value is not a required type TypeError is raised.
+    If input value is not in defined interval ValueError is raised.
+Keywords: enum, exception handling, multi type input
+"""
+
+from enum import Enum
+
+
+class Bloodtype(Enum):
+    ZERO_NEG = 0
+    ZERO_POS = 1
+    B_NEG = 2
+    B_POS = 3
+    A_NEG = 4
+    A_POS = 5
+    AB_NEG = 6
+    AB_POS = 7
+
+
+blood_type_text = {
+    "0-": Bloodtype.ZERO_NEG,
+    "0+": Bloodtype.ZERO_POS,
+    "B-": Bloodtype.B_NEG,
+    "B+": Bloodtype.B_POS,
+    "A-": Bloodtype.A_NEG,
+    "A+": Bloodtype.A_POS,
+    "AB-": Bloodtype.AB_NEG,
+    "AB+": Bloodtype.AB_POS,
+}
+
+# complete :
+def check_bt(donor, recipient):
+    """ Checks red blood cell compatibility based on 8 blood types
+        Args:
+        donor (int | str | Bloodtype): red blood cell type of the donor
+        recipient (int | str | Bloodtype): red blood cell type of the recipient
+        Returns:
+        bool: True for compatability, False otherwise.
+    """
+    # Validate arg type
+    if not isinstance(donor, (int, str, Bloodtype)):
+        raise TypeError
+    if not isinstance(recipient, (int, str, Bloodtype)):
+        raise TypeError
+
+    # Validate arg in range if arg is int
+    if isinstance(donor, int):
+        if donor not in range(0, 8):
+            raise ValueError
+    if isinstance(recipient, int):
+        if recipient not in range(0, 8):
+            raise ValueError
+    
+    # Validate arg in blood_type_test.keys() if arg is str
+    if isinstance(donor, str):
+        if donor not in blood_type_text.keys():
+            raise ValueError
+        else:
+            donor = blood_type_text[donor].value
+    if isinstance(recipient, str):
+        if recipient not in blood_type_text.keys():
+            raise ValueError
+        else:
+            recipient = blood_type_text[recipient]
+    
+    # Use int value of Bloodtype for compatibility check
+    if isinstance(donor, Bloodtype):
+        donor = donor.value
+    if isinstance(recipient, Bloodtype):
+        recipient = recipient.value
+
+    # Check compatibility
+    # Universal donor or universal recipient
+    if donor == Bloodtype.ZERO_NEG or recipient == Bloodtype.AB_POS:
+        return True
+    # if negative, there is no compatibility
+    elif -1 in _particular_antigen_comp(donor, recipient):
+        return False
+    else:
+        return True
+
+
+# hint
+def _particular_antigen_comp(donor: int, recipient: int) -> tuple:
+    """Returns a particular antigen compatibility, where each tuple member
+    marks a compatibility for a particular antigen  (A, B, Rh-D).
+    If tuple member is non-negative there is a compatibility.
+    For red blood cell compatibility is required that 
+    all tuple members are non-negative (i.e. compatibility for all 3 antigens).
+    0- bloodtype is represented as 0 ; AB+ is represented as 7; see Bloodtype enum
+    Examples:
+    _particular_antigen_comp(0, 7) -> (1, 1, 1)    0- can donate to AB+
+    _particular_antigen_comp(1, 3) -> (0, 1, 0)    0+ can donate to B+
+    _particular_antigen_comp(2, 5) -> (1, -1, 1)   B+ cannot donate to A+
+    _particular_antigen_comp(7, 0) -> (-1, -1, -1) AB+ cannot donate to 0-
+    """
+    return (
+        ((recipient // 4) % 2) - ((donor // 4) % 2),
+        ((recipient // 2) % 2) - ((donor // 2) % 2),
+        (recipient % 2) - (donor % 2),
+    )
