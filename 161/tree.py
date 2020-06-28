4a51b2b9eb318424ee617e0f2fba949de3ff8fb9
dcribb19 <daniel.cribb.10@gmail.com>
2020-06-28 12:03:15 -0400

    submission Bite 161 @codechalleng.es

--- 161/tree.py
+++ 161/tree.py
@@ -5,8 +5,6 @@ def count_dirs_and_files(directory='.') -> tuple:
     """Count the amount of of directories and files in passed in "directory" arg.
        Return a tuple of (number_of_directories, number_of_files)
     """
-    # dirs = len([item for item in os.listdir(directory) if os.path.isdir(os.path.join(directory, item))])
-    # files = len([item for item in os.listdir(directory) if os.path.isfile(os.path.join(directory, item))])
     dirs = 0
     files = 0
     for _, dirnames, filenames in os.walk(directory):
