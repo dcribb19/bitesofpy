67cbd144261ca4b7fd85accfdd893ed2ea3732c0
dcribb19 <daniel.cribb.10@gmail.com>
2020-07-11 19:35:27 -0400

    submission Bite 283 @codechalleng.es

--- 283/tomorrow.py
+++ 283/tomorrow.py
@@ -1,4 +1,8 @@
 from datetime import date, timedelta
 
-def tomorrow():
-    return date.today() + timedelta(days=1)
+def tomorrow(*args):
+    if len(args) == 0:
+        fake_today = date(2020, 7, 9)
+        return fake_today + timedelta(days=1)
+    else:
+        return args[0] + timedelta(days=1)
