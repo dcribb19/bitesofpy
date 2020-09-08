6a49c5395e1a7da2b6247ea4bcea26e6ebecf1a2
dcribb19 <daniel.cribb.10@gmail.com>
2020-09-07 20:02:08 -0400

    submission Bite 190 @codechalleng.es

---
+++ 190/income.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from urllib.request import urlretrieve
+from collections import defaultdict
+import xml.etree.ElementTree as ET
+
+# import the countries xml file
+tmp = Path(os.getenv("TMP", "/tmp"))
+countries = tmp / 'countries.xml'
+
+if not countries.exists():
+    urlretrieve(
+        'https://bites-data.s3.us-east-2.amazonaws.com/countries.xml',
+        countries
+    )
+
+
+def get_income_distribution(xml=countries):
+    """
+    - Read in the countries xml as stored in countries variable.
+    - Parse the XML
+    - Return a dict of:
+      - keys = incomes (wb:incomeLevel)
+      - values = list of country names (wb:name)
+    """
+    income_dist = defaultdict(list)
+    tree = ET.parse(countries)
+    root = tree.getroot()
+    for child in root:
+        income_dist[child[4].text].append(child[1].text)
+    return income_dist
