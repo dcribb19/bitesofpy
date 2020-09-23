a1e641c21e784144f194d80eebc80dcb07a857ba
dcribb19 <daniel.cribb.10@gmail.com>
2020-09-23 10:03:46 -0400

    submission Bite 118 @codechalleng.es

---
+++ 118/duplicates.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+
+def get_duplicate_indices(words):
+    """Given a list of words, loop through the words and check for each
+       word if it occurs more than once.
+       If so return the index of its first ocurrence.
+       For example in the following list 'is' and 'it'
+       occurr more than once, and they are at indices 0 and 1 so you would
+       return [0, 1]:
+       ['is', 'it', 'true', 'or', 'is', 'it', 'not?'] => [0, 1]
+       Make sure the returning list is unique and sorted in ascending order."""
+    c = Counter(words)
+    # Originally used generator, but tests only work for list.
+    return list(words.index(k) for k, v in c.items() if v > 1)
