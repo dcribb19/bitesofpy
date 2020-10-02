122ebd610472a7784896375fa6a4616e6e46fb8d
dcribb19 <daniel.cribb.10@gmail.com>
2020-10-01 21:13:28 -0400

    submission Bite 146 @codechalleng.es

---
+++ 146/rhombus.py
@@ -0,0 +1,31 @@
+STAR = '*'
+
+
+def gen_rhombus(width):
+    """Create a generator that yields the rows of a rhombus row
+       by row. So if width = 5 it should generate the following
+       rows one by one:
+
+       gen = gen_rhombus(5)
+       for row in gen:
+           print(row)
+
+        output:
+          *
+         ***
+        *****
+         ***
+          *
+    """
+    rows = []
+    for row in range(1, width + 1, 2):
+        padding = int((width - row) / 2)
+        rows.append(f"{' ' * padding}{STAR * row}{' ' * padding}")
+
+    bottom = rows.copy()
+    bottom.reverse()
+    bottom.pop(0)
+
+    rows = rows + bottom
+
+    return rows
