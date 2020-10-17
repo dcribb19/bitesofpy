725e3778fb7d78dfede04f16ddf35977a4b58e09
dcribb19 <daniel.cribb.10@gmail.com>
2020-10-17 13:16:51 -0400

    submission Bite 194 @codechalleng.es

--- code_challenges/194/fib.py
+++ code_challenges/194/fib.py
@@ -3,9 +3,7 @@ from functools import lru_cache
 
 @lru_cache
 def cached_fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    if n < 2:
+        return n
     else:
         return cached_fib(n - 1) + cached_fib(n - 2)
