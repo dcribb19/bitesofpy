f8357e4ba1fab30d3247bc84f6b4e8c14494229a
dcribb19 <daniel.cribb.10@gmail.com>
2020-10-18 17:01:35 -0400

    submission Bite 155 @codechalleng.es

---
+++ code_challenges/155/split.py
@@ -0,0 +1,18 @@
+import csv
+
+
+def split_words_and_quoted_text(text):
+    """Split string text by space unless it is
+       wrapped inside double quotes, returning a list
+       of the elements.
+
+       For example
+       if text =
+       'Should give "3 elements only"'
+
+       the resulting list would be:
+       ['Should', 'give', '3 elements only']
+    """
+    text = [text]
+    reader = csv.reader(text, delimiter=' ')
+    return next(reader)
