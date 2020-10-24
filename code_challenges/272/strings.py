5c9e1a59dabf86867ba1a4d5616c20a6cbfc2544
dcribb19 <daniel.cribb.10@gmail.com>
2020-10-24 17:14:56 -0400

    submission Bite 272 @codechalleng.es

---
+++ code_challenges/272/strings.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def common_words(sentence1: List[str], sentence2: List[str]) -> List[str]:
+    """
+    Input:  Two sentences - each is a list of words in case insensitive ways.
+    Output: Common words appearing in both sentences.
+            Capital and lowercase words are treated as the same word.
+
+            If there are duplicate words in the results, just choose one word.
+            Returned words should be sorted by word's length.
+    """
+    sentence1 = [x.lower() for x in sentence1]
+    sentence2 = [x.lower() for x in sentence2]
+    return sorted(list(set(sentence1) & set(sentence2)), key=lambda x: len(x))
