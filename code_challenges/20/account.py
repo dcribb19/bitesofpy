2d902c6b8e4716c9fc852733dfa36b1f8e3f7a96
dcribb19 <daniel.cribb.10@gmail.com>
2020-10-26 13:33:22 -0400

    submission Bite 20 @codechalleng.es

---
+++ code_challenges/20/account.py
@@ -0,0 +1,26 @@
+class Account:
+
+    def __init__(self):
+        self._transactions = []
+
+    @property
+    def balance(self):
+        return sum(self._transactions)
+
+    def __add__(self, amount):
+        self._transactions.append(amount)
+
+    def __sub__(self, amount):
+        self._transactions.append(-amount)
+
+    # add 2 dunder methods here to turn this class
+    # into a 'rollback' context manager
+    def __enter__(self):
+        # Make a copy of transactions for rollback.
+        self._copy_transactions = list(self._transactions)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Raise an exception to trigger a rollback.
+        if self.balance < 0:
+            self._transactions = self._copy_transactions
