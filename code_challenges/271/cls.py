70e31e15090fe86635362da93ab81a400f6bfce1
dcribb19 <daniel.cribb.10@gmail.com>
2020-10-31 08:28:32 -0400

    submission Bite 271 @codechalleng.es

--- code_challenges/271/cls.py
+++ code_challenges/271/cls.py
@@ -4,7 +4,5 @@ from string import ascii_uppercase
 
 def get_classes(mod):
     """Return a list of all classes in module 'mod'"""
-    members = inspect.getmembers(mod)
-    return [cls[0] for cls in members
-            if inspect.isclass(cls[1])
-            and cls[0][0] in ascii_uppercase]
+    return [cls[0] for cls in inspect.getmembers(mod, inspect.isclass)
+            if cls[0][0] in ascii_uppercase]
