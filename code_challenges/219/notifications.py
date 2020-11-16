58827ecf5a8904ca2a55de72587778c174544564
dcribb19 <daniel.cribb.10@gmail.com>
2020-11-16 09:28:51 -0500

    submission Bite 219 @codechalleng.es

--- code_challenges/219/notifications.py
+++ code_challenges/219/notifications.py
@@ -8,4 +8,4 @@ def gen_bite_planning(num_bites=1, num_days=1, start_date=TODAY):
     while True:
         for _ in range(num_bites):
             yield ret_date + timedelta(days=num_days)
-        ret_date = ret_date + timedelta(days=num_days)
+        ret_date += timedelta(days=num_days)
