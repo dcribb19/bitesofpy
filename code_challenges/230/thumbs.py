82cff10a86fe349ed78312bc4940537ae4c54845
dcribb19 <daniel.cribb.10@gmail.com>
2020-11-22 21:45:20 -0500

    submission Bite 230 @codechalleng.es

--- code_challenges/230/thumbs.py
+++ code_challenges/230/thumbs.py
@@ -15,13 +15,4 @@ class Thumbs:
             return f'{THUMBS_DOWN} ({abs(other)}x)'
 
     def __rmul__(self, other):
-        if other == 0:
-            raise ValueError('Specify a number')
-        elif other > 0 and other < 4:
-            return THUMBS_UP * other
-        elif other >= 4:
-            return f'{THUMBS_UP} ({other}x)'
-        elif other < 0 and other > -4:
-            return THUMBS_DOWN * abs(other)
-        else:
-            return f'{THUMBS_DOWN} ({abs(other)}x)'
+        return self.__mul__(other)
