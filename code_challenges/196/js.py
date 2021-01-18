7dd98c64f0737e512da91527d02db70349d54c82
dcribb19 <daniel.cribb.10@gmail.com>
2021-01-18 09:35:19 -0500

    submission Bite 196 @ codechalleng.es

---
+++ code_challenges/196/js.py
@@ -0,0 +1,19 @@
+class JsObject(dict):
+    """A Python dictionary that provides attribute-style access
+       just like a JS object:
+
+       obj = JsObject()
+       obj.cool = True
+       obj.foo = 'bar'
+
+       Try this on a regular dict and you get
+       AttributeError: 'dict' object has no attribute 'foo'
+    """
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        del self[name]
