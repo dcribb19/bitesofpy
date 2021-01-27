770d779c84380f84fc1cbb6aad1c39588b413550
dcribb19 <daniel.cribb.10@gmail.com>
2021-01-27 08:13:35 -0500

    submission Bite 236 @codechalleng.es

---
+++ code_challenges/236/files.py
@@ -0,0 +1,32 @@
+from pathlib import PosixPath
+import difflib
+
+
+def get_matching_files(directory: PosixPath, filter_str: str) -> list:
+    """Get all file names in "directory" and (case insensitive) match the ones
+       that exactly match "filter_str"
+
+       In case there is no exact match, return closely matching files.
+       If there are no closely matching files either, return an empty list.
+       (Return file names, not full paths).
+
+       For example:
+
+       d = Path('.')
+       files in dir: bite1 test output
+
+       get_matching_files(d, 'bite1') => ['bite1']
+       get_matching_files(d, 'Bite') => ['bite1']
+       get_matching_files(d, 'pybites') => ['bite1']
+       get_matching_files(d, 'test') => ['test']
+       get_matching_files(d, 'test2') => ['test']
+       get_matching_files(d, 'output') => ['output']
+       get_matching_files(d, 'o$tput') => ['output']
+       get_matching_files(d, 'nonsense') => []
+    """
+    file_names = [file.name for file in directory.iterdir()]
+    matching_names = [file for file in file_names
+                      if file.lower() == filter_str.lower()]
+    if not matching_names:
+        matching_names = difflib.get_close_matches(filter_str, file_names)
+    return matching_names
